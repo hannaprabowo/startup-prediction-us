8ca67088eb4f3b322f94f9650a6a2910da29b835
hannaprabowo <157619808+hannaprabowo@users.noreply.github.com>
2024-10-30 14:21:48 +0100

    update

--- final.R
+++ final.R
@@ -39,5 +39,38 @@ final_data2$status <- mapvalues(final_data2$status,
                                 to = c(1,0)) # 1 = acquired, 0 = close
 final_data2$status <- as.factor(final_data2$status) # change to factor for target variable
 
+## convert the one hot encoding
+
+# state
+state <- as.factor(ifelse(final_data2$is_CA == 1, "CA",
+                          ifelse(final_data2$is_NY == 1, "NY",
+                                 ifelse(final_data2$is_MA == 1, "MA",
+                                        ifelse(final_data2$is_TX == 1, "TX",
+                                               ifelse(final_data2$is_otherstate == 1, "Other State", "Other State"))))))
+# industry
+industry <- as.factor(ifelse(final_data2$is_software == 1, "Software",
+                             ifelse(final_data$is_web == 1, "Web",
+                                    ifelse(final_data2$is_mobile == 1, "Mobile",
+                                           ifelse(final_data2$is_enterprise == 1, "Enterprise",
+                                                  ifelse(final_data2$is_advertising == 1, "Advertising",
+                                                         ifelse(final_data$is_gamesvideo == 1, "Video Games",
+                                                                ifelse(final_data$is_ecommerce == 1, "E-Commerce",
+                                                                       ifelse(final_data2$is_biotech == 1, "Biotech",
+                                                                              ifelse(final_data2$is_consulting == 1, "Consulting",
+                                                                                     ifelse(final_data2$is_othercategory == 1, "Other Category",0)))))))))))
+
+# add categorical factors
+final_data2$industry <- industry
+final_data2$state <- state
+
+# remove one hot encoding
+final_data2 <- final_data2[,-c(9:24)]
+
+# divide train and test set, 70 -30 split #
+set.seed(1000)
+index <- createDataPartition(final_data2$status, p = .7, list = FALSE)
+train <- final_data2[ index,]
+test  <- final_data2[-index,]
+
 
 
